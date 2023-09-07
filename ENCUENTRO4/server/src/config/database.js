067214eb91e38028fbba3eff7fcdb110842e9691
agustin <agustinmcenturion@gmail.com>
2023-09-07 17:03:31 -0300

    clase sincronica unidad 4 - encuentro 4 - 05/09 - ACTUALIZADO 07/09

--- ENCUENTRO4/server/src/config/database.js
+++ ENCUENTRO4/server/src/config/database.js
@@ -1,17 +1,17 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('db_task', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+export const sequelize = new Sequelize("db_task", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export const startDb = async () => {
-    try {
-        await sequelize.authenticate();
-        // await sequelize.sync({ force: true });
-        await sequelize.sync();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+  try {
+    await sequelize.authenticate();
+    // await sequelize.sync({ force: true });
+    await sequelize.sync();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
--- ENCUENTRO4/web/script.js
+++ ENCUENTRO4/web/script.js
@@ -1,60 +1,57 @@
-const contenedor = document.getElementById('container-row')
-const btnCrear = document.getElementById('btn-new')
-const myModal = new bootstrap.Modal(document.getElementById('myModal'))
-const btnSave = document.getElementById('btn-save')
+const contenedor = document.getElementById("container-row");
+const btnCrear = document.getElementById("btn-new");
+const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+const btnSave = document.getElementById("btn-save");
+const form = document.getElementById("formulario");
 
+let html = "";
+let option = "";
+let idForm = "";
 
-let html = ''
-let option = ''
+const inputTitle = document.getElementById("inputTitle");
+const inputDescription = document.getElementById("inputDescription");
+const inputPoster = document.getElementById("inputPoster");
 
-const inputTitle = document.getElementById('inputTitle')
-const inputDescription = document.getElementById('inputDescription')
-const inputPoster = document.getElementById('inputPoster')
+btnCrear.addEventListener("click", () => {
+  option = "new";
+  btnSave.textContent = "new";
+  inputTitle.value = "";
+  inputDescription.value = "";
+  inputPoster.value = "";
+  myModal.show();
+});
 
-btnCrear.addEventListener('click', () => {
-    option = "new"
-    btnSave.textContent = "New"
-    inputTitle.value = ""
-    inputDescription.value = ""
-    inputPoster.value = ""
-    myModal.show()
-})
+document.addEventListener("click", (event) => {
+  if (event.target.matches("#btn-delete")) {
+    const article = event.target.closest(".col-4");
+    const idArticle = article.dataset.id;
 
-document.addEventListener('click', (event) => {
-    if (event.target.matches('#btn-delete')) {
-        const article = event.target.closest('.col-4')
-        const idArticle = article.dataset.id
-
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                fetch(`http://localhost:3000/api/tasks/${idArticle}`, {
-                    method: 'DELETE'
-                }).then(res => {
-                    if (res.ok) {
-                        article.remove()
-                    }
-                }).catch(err => {
-                    console.error(err)
-                })
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                )
-            }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/api/tasks/${idArticle}`, {
+          method: "DELETE",
         })
-
-
-    }
-})
+          .then((res) => {
+            if (res.ok) {
+              article.remove();
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  }
+});
 // document.addEventListener('click', (event) => {
 //     if (event.target.matches('#btn-delete')) {
 //         const article = event.target.closest('.col-4')
@@ -72,14 +69,85 @@ document.addEventListener('click', (event) => {
 //     }
 // })
 
+document.addEventListener("click", (event) => {
+  if (event.target.matches("#btn-edit")) {
+    const article = event.target.closest(".col-4");
 
+    const idArticle = article.dataset.id;
+    const urlPosterEdit = article.children[0].children[0].src;
+    const titleEdit = article.children[0].children[1].children[0].textContent;
+    const descriptionEdit =
+      article.children[0].children[1].children[1].textContent;
 
-fetch('http://localhost:3000/api/tasks')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(task => {
-            html += `
+    idForm = idArticle;
+    inputTitle.value = titleEdit;
+    inputDescription.value = descriptionEdit;
+    inputPoster.value = urlPosterEdit;
+    option = "edit";
+    btnSave.textContent = "Edit";
+    myModal.show();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log("submit");
+
+  if (option === "new") {
+    const newTask = {
+      title: inputTitle.value,
+      description: inputDescription.value,
+      poster: inputPoster.value,
+    };
+
+    fetch('http://localhost:3000/api/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTask)
+    }).then(res => {
+      console.log(res)
+        if (res.ok) {
+          alert("Task created successfully");
+          myModal.hide();
+          location.reload();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (option === "edit") {
+    const newTask = {
+      title: inputTitle.value,
+      description: inputDescription.value,
+      poster: inputPoster.value,
+    };
+
+    fetch(`http://localhost:3000/api/tasks/${idForm}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTask)
+    }).then(res => {
+      if(res.ok){
+        alert('Task edited successfully')
+        myModal.hide();
+        location.reload();
+      }
+    })
+  }
+});
+
+fetch("http://localhost:3000/api/tasks")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    data.forEach((task) => {
+      html += `
             <article class="col-4 d-flex justify-content-center mb-3" data-id="${task.id}">
                 <div class="card" style="width: 18rem;">
                     <img src="${task.poster}"
@@ -94,10 +162,8 @@ fetch('http://localhost:3000/api/tasks')
                     </div>
                 </div>
             </article>
-            `
+            `;
 
-            contenedor.innerHTML = html;
-        });
-    })
-
-
+      contenedor.innerHTML = html;
+    });
+  });
